b72e4d8222a8bb7996547f6af7e8a6be47531a3d
sumakubas <sumakubasad66@gmail.com>
2024-02-10 11:39:19 +0530

    code department details

---
+++ department.java
@@ -0,0 +1,14 @@
+
+class department {
+    String dept_name;
+
+    void dept(int n) {
+        if (n == 1) {
+            System.out.println("Testing department");
+        } else if (n == 2) {
+            System.out.println("Developing department");
+        } else {
+            System.out.println("Research department");
+        }
+    }
+}
